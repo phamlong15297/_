26573a1c4b7fda027a2918f332d05a52db47dbf5
phamlong15297 <32757289+phamlong15297@users.noreply.github.com>
2025-07-24 18:32:13 +0900

    .

--- sample-node/eslint.config.mjs
+++ sample-node/eslint.config.mjs
@@ -1,11 +1,24 @@
-import js from "@eslint/js";
-import globals from "globals";
-import pluginVue from "eslint-plugin-vue";
-import { defineConfig } from "eslint/config";
-
+import js from '@eslint/js';
+import globals from 'globals';
+import pluginVue from 'eslint-plugin-vue';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,vue}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,vue}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
-  pluginVue.configs["flat/essential"],
+  {
+    files: ['**/*.{js,mjs,cjs,vue}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,vue}'],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
+  pluginVue.configs['flat/essential'],
+  {
+    files: ['**/*.vue'],
+    rules: {
+      ...pluginVue.configs['flat/essential'].rules,
+      'vue/multi-word-component-names': 'off',
+    },
+  },
 ]);
