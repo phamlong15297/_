78bcdd14e30060fd42416c4993406d2291f707a8
phamlong15297 <32757289+phamlong15297@users.noreply.github.com>
2025-07-24 06:40:38 +0000

    add js

---
+++ sample-node/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    // your custom rules
+  },
+};
---
+++ sample-node/bad.ts
@@ -0,0 +1,3 @@
+const foo =  {   bar: "baz",qux:[1,2,3] }
+function sayHello( name:string ){console.log( "Hello, "+name );}
+sayHello("World")
