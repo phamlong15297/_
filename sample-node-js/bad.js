6211d57478b9a3e2fcad4e03842005d5e6a44d3f
phamlong15297 <32757289+phamlong15297@users.noreply.github.com>
2025-07-25 05:44:11 +0000

    prettier ok

--- sample-node-js/bad.js
+++ sample-node-js/bad.js
@@ -1,7 +1,7 @@
-const foo=   (x ,y)=>{
-console.log( 'sum is:'+x+y )
-}
+const foo = (x, y) => {
+  console.log('sum is:' + x + y);
+};
 
-var unused = 42
+var unused = 42;
 
-foo(1,'2')
+foo(1, '2');
